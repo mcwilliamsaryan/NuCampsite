6513e3dbd1c709d99fb81c05df705e2cde3fff22
Ryan McWilliams <cras.rmcwilliams@gmail.com>
2022-09-01 19:09:28 -0700

    Contact Page

---
+++ src/features/campsites/campsitesSlice.js
@@ -0,0 +1,14 @@
+import { CAMPSITES } from '../../app/shared/CAMPSITES';
+import CampsiteDetail from './CampsiteDetail';
+
+export const selectAllCampsites = () => {
+  return CAMPSITES;
+};
+
+export const selectCampsiteById = (id) => {
+  return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
+};
+
+export const selectFeaturedCampsite = () => {
+  return CAMPSITES.find((campsite) => campsite.featured);
+};
